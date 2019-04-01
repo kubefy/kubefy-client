b96ed2c34e0f74b7eeccf8a2a46b3cc25708b9e7
Huamin Chen <hchen@redhat.com>
2019-04-01 14:23:05 -0400

    init
    Signed-off-by: Huamin Chen <hchen@redhat.com>

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+'use strict';
+
+const cmd = require('./lib/cmd')
+
+const args = require('yargs').argv;
+switch(args.option.toLowerCase()){
+case "deploy":
+    cmd.createFunction();
+    break;
+case "createuser":
+    cmd.createUser();
+    break;
+default:
+    console.log("invalid option: " + args.option);
+}
